93e6b52dcd48f7c26a320057a471e47752b01154
nanut <galunyu@gmail.com>
2020-12-26 16:53:53 +0700

    [Added] prductRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from "colors"
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -13,14 +13,8 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/prodcuts/:id', (req, res) => {
-    const product = products.find((product) => product._id === req.params.id)
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
