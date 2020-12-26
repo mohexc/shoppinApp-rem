404fffa9fa4036ce5f5c662092152383c31afc48
nanut <galunyu@gmail.com>
2020-12-26 22:56:23 +0700

    [Added] userRouter, bcryptjs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,17 @@
 import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
 
+// @desc        Fetch all products
+// @route       GET /api/products
+// @access      Public
 const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({})
 
     res.json(products)
 })
 
+// @desc        Fetch single product
+// @route       Get /api/products/:id
+// @access      Public
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
@@ -15,7 +20,7 @@ const getProductById = asyncHandler(async (req, res) => {
     } else {
         res.status(404).json({ message: "Product not found" })
     }
-    res.json(product)
+
 })
 
 export { getProducts, getProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,17 +2,7 @@ import express from 'express'
 import { getProducts, getProductById } from "../controllers/productController.js"
 const router = express.Router()
 
-
-
-// @desc        Fetch all products
-// @route       GET /api/products
-// @access      Public
 router.route('/').get(getProducts)
-
-
-// @desc        Fetch single product
-// @route       Get /api/products/:id
-// @access      Public
 router.route('/:id').get(getProductById)
 
 export default router
